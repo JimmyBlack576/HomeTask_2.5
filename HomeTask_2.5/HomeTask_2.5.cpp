220a99d3321637118c0a9591a9c50ea03fa00cbe
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-14 23:03:48 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_2.5/HomeTask_2.5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int n1, n2, n3, temp, finl=0;
+    for (int i = 100; i <= 999; i++) {
+        n3 = i % 10;
+        temp = i / 10;
+        n2 = temp % 10;
+        n1 = temp / 10;
+
+        if ((n1 != n2) && (n1 != n3) && (n2 != n3)) {
+            finl++;
+        }
+    }
+    cout << "Значений с разными числами: " << finl;
+}
+
